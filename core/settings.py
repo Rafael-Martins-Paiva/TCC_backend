c60a32c70d4994ae0e57dcb44cbe44e20d6de841
NAME <mail@gmail.com>
2025-06-29 17:13:25 -0300

    sqlite-postegresql

--- core/settings.py
+++ core/settings.py
@@ -59,8 +59,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tcc_system',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
